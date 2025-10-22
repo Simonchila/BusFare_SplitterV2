516cbdc90b4b2d5223d8d737f47b95d08fdb992b
simon <simonchila47@gmail.com>
2025-10-22 23:13:28 +0200

    done

--- app/src/main/java/com/example/busfare_splitterv2/UI/SplashActivity.java
+++ app/src/main/java/com/example/busfare_splitterv2/UI/SplashActivity.java
@@ -2,12 +2,9 @@ package com.example.busfare_splitterv2.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.busfare_splitterv2.R;
-
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,12 +13,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Button btnGetStarted = findViewById(R.id.btnGetStarted);
-
-
-        btnGetStarted.setOnClickListener(v -> {
-            startActivity(new Intent(SplashActivity.this, GroupActivityMsg.class));
-            finish();
-        });
+        // Automatically move to next activity after 3 seconds
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish(); // close splash so user can’t go back to it
+        }, 3000); // delay = 3 seconds (3000ms)
     }
 }
+
